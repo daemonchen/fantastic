d841997d4b9119a268f03a90036cf2ad5a103861
悠歌 <guangyuan.cgy@alibaba-inc.com>
2014-08-13 16:39:16 +0800

    add  loading stuff

---
+++ public/js/common/sidebar.js
@@ -0,0 +1,13 @@
+fantastic.controller('ArchiveSidebarController', function($scope, $http, $log, _){
+
+  $scope.loading = true;
+
+
+})
+
+fantastic.controller('TagSidebarController', function($scope, $http, $log, _){
+
+  $scope.loading = true;
+
+
+})
