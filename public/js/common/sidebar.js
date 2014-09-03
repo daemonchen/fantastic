fb684b978b05ebc6ef1bd825424831d7e398008e
悠歌 <guangyuan.cgy@alibaba-inc.com>
2014-09-03 17:32:41 +0800

    rework js

--- public/js/common/sidebar.js
+++ public/js/common/sidebar.js
@@ -1,32 +1,49 @@
-fantastic.controller('ArchiveSidebarController', function($scope, $http, $log, _, postService){
+fantastic.controller('ArchiveSidebarController', function($scope, $http, $log, _, postService) {
 
-  $scope.loading = true;
-  $log.info(postService.getPosts());
+    $scope.loading = true;
+    $log.info(postService.getPosts());
 
 })
 
-fantastic.controller('TagSidebarController', function($scope, $http, $log, _){
+fantastic.controller('TagSidebarController', function($scope, $http, $log, _) {
 
-  $scope.loading = true;
-  var logError = function(data, status) {
-    $log.log('code '+status+': '+data);
-  };
-  $scope.loading = true;
-  var init = function() {
-    return $http.get('/tag/getAllTags').
-      success(function(data) {
-        if (!!data && data.length != 0 && data != "null") {
+    $scope.loading = true;
+    $scope.logError = function(data, status) {
+        $log.log('code ' + status + ': ' + data);
+    };
+    $scope.loading = true;
+
+    var tagUtil = {
+        init: function() {
+            this.getAllTags();
+        },
+        getAllTags: function() {
+            var self = this;
+            $http.get('/tag/getAllTags').
+            success(function(data) {
+                (!!data) && (data.length != 0) && (data != "null") && self.renderTags(data);
+            }).
+            error($scope.logError);
+
+        },
+        renderTags: function(data) {
             var tagArray = _.uniq(_.pluck(data, 'Tag'));
             $scope.tags = [];
             for (var i = tagArray.length - 1; i >= 0; i--) {
-                $scope.tags.push({tag: tagArray[i], count: _.where(data, {Tag: tagArray[i]}).length})
+                $scope.tags.push({
+                    tag: tagArray[i],
+                    count: _.where(data, {
+                        Tag: tagArray[i]
+                    }).length
+                })
             };
             $scope.loading = false;
 
-        };
-      }).
-      error(logError);
-  };
-  init();
+
+        }
+    }
+
+
+    tagUtil.init();
 
 })
