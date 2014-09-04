70e720c0bbfbb348cf058f7757e6a8b0e838a0de
悠歌 <guangyuan.cgy@alibaba-inc.com>
2014-09-04 10:50:11 +0800

    delete useless code && add links

--- public/js/post.js
+++ public/js/post.js
@@ -1,15 +1,5 @@
 
 $(function(){
-
-  var stamp = $("#stamp").html()
-
-  var formatTimeLabel = function(){
-    var stampLabel = parseInt(stamp)
-    $("#time").html(moment(stampLabel*1000).format("YYYY年MM月DD日  HH:mm:ss"))
-  }
-
-  formatTimeLabel()
-
   var doPostUpdate = function(url, stamp, articleContent){
     $.ajax({
         url: url,
@@ -18,23 +8,6 @@ $(function(){
         dataType: "json",
         success: function (result) {
           console.log("todo: change url to detail","result:",result);
-        },
-        error: function (xhr, ajaxOptions, thrownError) {
-        console.log(xhr.status);
-        console.log(thrownError);
-        }
-    });
-  }
-  var doPostComment = function(url, commentData){
-    $.ajax({
-        url: url,
-        type: "POST",
-        data: { commentData: JSON.stringify(commentData) },
-        dataType: "json",
-        success: function (result) {
-          console.log("doPostComment result:",result);
-          window.location.reload();
-          window.localStorage.clear();
         },
         error: function (xhr, ajaxOptions, thrownError) {
         console.log(xhr.status);
@@ -74,62 +47,7 @@ $(function(){
     console.log(stamp)
     doDeleteUpdate("/post/index/?stamp="+stamp)
   });
-  //comments
-
-  var editorComment = CKEDITOR.replace( 'comment' );
-
-  // localStorage user comment stuff
-  $("#username").keyup(function(){
-    window.localStorage.setItem("edittingUsername",$(this).val());
-  });
-
-  $("#email").keyup(function(){
-    window.localStorage.setItem("edittingEmail",$(this).val());
-  });
-
-  editorComment.on("key",function(evt){
-    console.log("data when click:",evt.editor.getData())
-    window.localStorage.setItem("edittingCommentContent",evt.editor.getData());
-  })
-
-  var userName = window.localStorage.getItem("edittingUsername");
-  var userEmail = window.localStorage.getItem("edittingEmail");
-  var userComment = window.localStorage.getItem("edittingCommentContent");
-  if(userName&&userName.length>0){
-    $("#username").val(userName)
-  }
-  if(userEmail&&userEmail.length>0){
-    $("#email").val(userEmail)
-  }
-  if(userComment&&userComment.length>0){
-    editorComment.setData(userComment)
-  }
 
 
-  $(".submit").click(function(){
-    var commentData = {
-      relativeStamp: stamp,
-      userName: $("#username").val() || "游客",
-      userEmail: $("#email").val(),
-      commentText: editorComment.getData(),
-      commentTime: new Date().valueOf().toString()
-    }
-    if(commentData.commentText.length == 0){
-      return console.log("你在逗我么");
-    }
-    doPostComment("/post/comment",commentData)
-  });
-
-  // transform comment result
-  $(".comment .avatar img").each(function(index){
-    var item = $(".comment .avatar img").eq(index);
-    item.attr("src","http://www.gravatar.com/avatar/" + MD5(item.attr("data")))
-  });
-
-  $(".comment .content .metadata .date").each(function(index){
-    var item = $(".comment .content .metadata .date").eq(index);
-    var time = parseInt(item.html());
-    item.html(moment(time).fromNow())
-  });
 
 });
