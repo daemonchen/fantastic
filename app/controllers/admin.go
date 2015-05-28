30095851f480c811e4f99c02cf29c71a68ac88bb
悠歌 <guangyuan.cgy@alibaba-inc.com>
2015-05-28 16:16:58 +0800

    ui stuff

--- app/controllers/admin.go
+++ app/controllers/admin.go
@@ -1,55 +1,55 @@
 package controllers
 
 import (
-	"fantastic/app/models"
-	// "fmt"
-	"crypto/md5"
-	"fmt"
-	"github.com/jgraham909/revmgo"
-	"github.com/revel/revel"
-	"labix.org/v2/mgo/bson"
+    "fantastic/app/models"
+    // "fmt"
+    "crypto/md5"
+    "fmt"
+    "github.com/jgraham909/revmgo"
+    "github.com/revel/revel"
+    "labix.org/v2/mgo/bson"
 )
 
 type Admin struct {
-	*revel.Controller
-	revmgo.MongoController
+    *revel.Controller
+    revmgo.MongoController
 }
 
 type Result struct {
-	status string
-	data   string
+    status string
+    data   string
 }
 
 func (c Admin) Index() revel.Result {
-	return c.Render()
+    return c.Render()
 
 }
 
 func (c Admin) Login(username string, password string) revel.Result {
-	responseJson := &Result{}
-	user := models.GetUserByName(c.MongoSession, username)
-	if password == user.Password {
-		c.Response.Status = 200
-		c.Session["islogin"] = "true"
-		return c.RenderJson(responseJson)
-	} else {
-		responseJson = &Result{"caicaikana", "login failed"}
-		c.Response.Status = 403
-		c.Session["islogin"] = "false"
-		return c.RenderJson(responseJson)
+    responseJson := &Result{}
+    user := models.GetUserByName(c.MongoSession, username)
+    if password == user.Password {
+        c.Response.Status = 200
+        c.Session["islogin"] = "true"
+        return c.RenderJson(responseJson)
+    } else {
+        responseJson = &Result{"caicaikana", "login failed"}
+        c.Response.Status = 403
+        c.Session["islogin"] = "false"
+        return c.RenderJson(responseJson)
 
-	}
+    }
 }
 
 func (c Admin) Register(username string, password string) revel.Result {
-	pwd := fmt.Sprintf("%x", md5.Sum([]byte(password)))
-	user := &models.User{bson.NewObjectId(), username, pwd}
-	err := user.Save(c.MongoSession)
-	if err != nil {
-		panic(err)
-		return c.RenderJson(&Result{"failed", "err"})
-	} else {
-		revel.INFO.Println("register success")
-		return c.RenderJson(&Result{"success", "register"})
-	}
+    pwd := fmt.Sprintf("%x", md5.Sum([]byte(password)))
+    user := &models.User{bson.NewObjectId(), username, pwd}
+    err := user.Save(c.MongoSession)
+    if err != nil {
+        panic(err)
+        return c.RenderJson(&Result{"failed", "err"})
+    } else {
+        revel.INFO.Println("register success")
+        return c.RenderJson(&Result{"success", "register"})
+    }
 }
--- app/controllers/post.go
+++ app/controllers/post.go
@@ -1,75 +1,75 @@
 package controllers
 
 import (
-	// "encoding/json"
-	"crypto/md5"
-	"fantastic/app/models"
-	"github.com/jgraham909/revmgo"
-	"github.com/revel/revel"
-	// "github.com/revel/revel/cache"
-	"io"
-	// "labix.org/v2/mgo/bson"
-	// "fmt"
-	"math/rand"
-	"strconv"
-	"time"
+    // "encoding/json"
+    "crypto/md5"
+    "fantastic/app/models"
+    "github.com/jgraham909/revmgo"
+    "github.com/revel/revel"
+    // "github.com/revel/revel/cache"
+    "io"
+    // "labix.org/v2/mgo/bson"
+    // "fmt"
+    "math/rand"
+    "strconv"
+    "time"
 )
 
 type Post struct {
-	*revel.Controller
-	revmgo.MongoController
+    *revel.Controller
+    revmgo.MongoController
 }
 
 var CommentCache = make(map[string]string)
 
 func (c *Post) generateSessionKey() []byte {
-	md5Key := md5.New()
-	io.WriteString(md5Key, "this is my first hash session key")
-	return md5Key.Sum([]byte("daemonchen"))
+    md5Key := md5.New()
+    io.WriteString(md5Key, "this is my first hash session key")
+    return md5Key.Sum([]byte("daemonchen"))
 
 }
 func (c *Post) Index() revel.Result {
-	controllerName := "home"
-	isLogin := c.Session["islogin"]
+    controllerName := "home"
+    isLogin := c.Session["islogin"]
 
-	randNum := rand.Int63n(time.Now().Unix())
-	hashKey := c.generateSessionKey()
-	c.Session[string(hashKey[:])] = strconv.FormatInt(randNum, 10)
-	CommentCache[strconv.FormatInt(randNum, 10)] = "true"
-	return c.Render(controllerName, isLogin)
+    randNum := rand.Int63n(time.Now().Unix())
+    hashKey := c.generateSessionKey()
+    c.Session[string(hashKey[:])] = strconv.FormatInt(randNum, 10)
+    CommentCache[strconv.FormatInt(randNum, 10)] = "true"
+    return c.Render(controllerName, isLogin)
 
 }
 func (c *Post) GetPostByStamp(stamp string) revel.Result {
-	post := models.GetPostByStamp(c.MongoSession, stamp)
+    post := models.GetPostByStamp(c.MongoSession, stamp)
 
-	return c.RenderJson(post)
+    return c.RenderJson(post)
 
 }
 func (c *Post) Delete(stamp string) revel.Result {
-	// controllerName := "home"
-	if isLogin := c.Session["islogin"]; isLogin == "true" {
-		err := models.DeletePost(c.MongoSession, stamp)
-		if err != nil {
-			panic(err)
-		}
-	}
-	return c.RenderJson(&BayesLearnResult{stamp, "success update"})
+    // controllerName := "home"
+    if isLogin := c.Session["islogin"]; isLogin == "true" {
+        err := models.DeletePost(c.MongoSession, stamp)
+        if err != nil {
+            panic(err)
+        }
+    }
+    return c.RenderJson(&BayesLearnResult{stamp, "success update"})
 }
 
 func (c *Post) Update(stamp string, content string) revel.Result {
-	revel.WARN.Println("commentData host:", c.Request.RemoteAddr)
-	responseJson := &BayesLearnResult{"delete", "success delete"}
-	err := models.UpdatePost(c.MongoSession, stamp, content)
-	if err != nil {
-		revel.WARN.Println("occur err when update:", err)
-	}
-	revel.WARN.Println("post updated success")
-	return c.RenderJson(responseJson)
+    revel.WARN.Println("commentData host:", c.Request.RemoteAddr)
+    responseJson := &BayesLearnResult{"delete", "success delete"}
+    err := models.UpdatePost(c.MongoSession, stamp, content)
+    if err != nil {
+        revel.WARN.Println("occur err when update:", err)
+    }
+    revel.WARN.Println("post updated success")
+    return c.RenderJson(responseJson)
 }
 
 func (c *Post) clearCommentCacheValue() {
-	hashKey := c.generateSessionKey()
-	CommentCache[c.Session[string(hashKey[:])]] = "diff"
+    hashKey := c.generateSessionKey()
+    CommentCache[c.Session[string(hashKey[:])]] = "diff"
 }
 
 // func (c *Post) AddComment(commentData string) revel.Result {
